0b30bc4dea00ad27a0f16fa69258c2b2039fa170
Kim <99856852+KBryant702@users.noreply.github.com>
2022-08-14 13:36:51 -0700

    State and Outputting Dynamic Lists

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+// all react hooks start with use
+import { useState } from 'react'
 
 function App() {
+ const [name, setName] = useState('Mario')
+ const [events, setEvents] = useState([
+  {title: "Mario's Birthday Bash", id: 1},
+  {title: "Bowser's Live Stream", id:2},
+  {title: "Race on Moo Moo Farm", id: 3}
+ ])
+
+  const handleClick= ()=> {
+    setName('Luigi')
+    console.log(name)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick ={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={events.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }

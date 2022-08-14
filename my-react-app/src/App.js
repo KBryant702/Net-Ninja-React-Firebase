3f95a9c211f63ef9444b94ca12cb58c4e15703df
Kim <99856852+KBryant702@users.noreply.github.com>
2022-08-14 13:55:03 -0700

    Using the Previous State

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,31 +1,31 @@
+import {useState} from 'react'
 
 import './App.css';
-// all react hooks start with use
-import { useState } from 'react'
+
 
 function App() {
- const [name, setName] = useState('Mario')
  const [events, setEvents] = useState([
   {title: "Mario's Birthday Bash", id: 1},
   {title: "Bowser's Live Stream", id:2},
   {title: "Race on Moo Moo Farm", id: 3}
  ])
 
-  const handleClick= ()=> {
-    setName('Luigi')
-    console.log(name)
-  }
-  return (
-    <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick ={handleClick}>Change name</button>
-      {events.map((event, index) => (
-        <div key={events.id}>
-          <h2>{index} - {event.title}</h2>
-        </div>
-      ))}
-    </div>
-  );
+ const handleClick = (id) => {
+  setEvents(prevEvents => {
+    return prevEvents.filter(event => id !== event.id)
+  })
+}
+
+return (
+  <div className="App">
+    {events.map((event, index) => (
+      <div key={event.id}>
+        <h2>{index} - {event.title}</h2>
+        <button onClick={() => handleClick(event.id)}>delete event</button>
+      </div>
+    ))}
+  </div>
+);
 }
 
 export default App;
